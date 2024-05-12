472350c1329c2e1ce080899aaacf11a8183f1f0e
mrayenn <rayen.merghad@outlook.com>
2024-05-12 22:18:20 +0100

    fix: outputs

--- infra/modules/roles/outputs.tf
+++ infra/modules/roles/outputs.tf
@@ -1,5 +1,5 @@
 output "eks_node_role" {
-  value = aws_iam_role.eks_node_role
+  value = aws_iam_role.eks_node_role.id
 }
 output "eks_cluster_role_arn" {
   value = aws_iam_role.eks_cluster_role.arn
@@ -10,11 +10,11 @@ output "eks_node_role_arn" {
 }
 
 output "eks_cluster_policy" {
-  value = aws_iam_role_policy_attachment.eks_cluster_policy
+  value = aws_iam_role_policy_attachment.eks_cluster_policy.id
 }
 
 output "eks_vpc_resource_controller" {
-  value = aws_iam_role_policy_attachment.eks_vpc_resource_controller
+  value = aws_iam_role_policy_attachment.eks_vpc_resource_controller.id
 }
 
 
