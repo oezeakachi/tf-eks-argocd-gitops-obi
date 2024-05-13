28af607ee1c221588628f4612930444e3caed952
mrayenn <rayen.merghad@outlook.com>
2024-05-13 12:53:56 +0100

    fix: provider root module

---
+++ infra/modules/eks/outputs.tf
@@ -0,0 +1,11 @@
+output "cluster_endpoint" {
+  value = data.aws_eks_cluster.cluster.endpoint
+}
+
+output "cluster_certificate_authority" {
+  value = data.aws_eks_cluster.cluster.certificate_authority[0].data
+}
+
+output "cluster_auth_token" {
+  value = data.aws_eks_cluster_auth.cluster.token
+}
