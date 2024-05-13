c7080a300cd00f9c45b1480a7547b66e82728455
mrayenn <rayen.merghad@outlook.com>
2024-05-13 16:01:43 +0100

    fix: outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,15 +1,19 @@
 output "eks_cluster_endpoint" {
   value = module.eks.cluster_endpoint
+  sensitive = true
 }
 
 output "eks_cluster_name" {
   value = module.eks.cluster_name
+  sensitive = true
 }
 
 output "cluster_certificate_authority" {
   value = module.eks.cluster_certificate_authority
+  sensitive = true
 }
 
 output "cluster_auth_token" {
   value = module.eks.cluster_auth_token
+  sensitive = true
 }
