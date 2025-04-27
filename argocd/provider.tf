2a41ffe69f039a7a1f6acc1207cd5e8be9aa3f5b
oezeakachi <oezeakac@redhat.com>
2025-04-27 16:40:41 +0100

    ameneded bucket name

--- argocd/provider.tf
+++ argocd/provider.tf
@@ -13,7 +13,7 @@ terraform {
 data "terraform_remote_state" "infra" {
   backend = "s3"
   config = {
-    bucket = "remote-rayen-tfstate-obi"
+    bucket = "remote-tfstate-obi"
     key    = "infra/terraform.tfstate"
     region = "eu-west-1"
   }
